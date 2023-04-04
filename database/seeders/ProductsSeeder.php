9481b4bd6bb0187eef677c8e4f773d4bbb500d74
Taha Zargar <mr.tahazargar@gmail.com>
2023-04-04 06:17:07 +0430

    V 1.1

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductsSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class ProductsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            'name' => 'Apple iphone x',
+            'price' => '1000$',
+            'description' => 'This mobile is so fast and fantastic',
+            'category' => 'mobile',
+            'gallery' => 'https://s3-storage.textopus.nl/wp-content/uploads/2017/09/12212815/iPhone-X-voorkant-water.jpg',
+        ]);
     }
 }
